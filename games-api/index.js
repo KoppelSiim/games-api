a676a2ed853d9b09fcb9b70e293d6e4382a807dd
KoppelSiim <siimk999@gmail.com>
2023-12-21 15:38:14 +0200

    siim branch first commit

--- games-api/index.js
+++ games-api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 //const port = 8080
+
+// siim branch first commit
 const port = process.env.APP_PORT
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs');
