2cd27533edfe11f0377f8a3f9b8c1e8b6b1d49f3
Siim Koppel <siimk999@gmail.com>
2024-01-22 21:07:00 +0200

    orders controller

---
+++ games-api/controllers/ordersController.js
@@ -0,0 +1,9 @@
+const {db} = require("../db")
+const Profile= db.profiles
+
+getBaseUrl = (request) => {
+    return (
+        (request.connection && request.connection.encryption ? "https" : "http") +
+        `://${request.headers.host}`
+    )
+}
